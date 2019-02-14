a97fd55c7b36a35ce8cbbc030b09073b934b6cbf
attila5287 <42498753+attila5287@users.noreply.github.com>
2019-02-14 04:44:25 -0700

    asdasd

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/main')
+def simulator(foo):
+    return render_template('main.html')
+
+if __name__ == '__main__':
+  app.run(debug=True)
+ 
