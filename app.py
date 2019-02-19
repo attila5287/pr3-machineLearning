7803e0026b39e724f4845b2bb8897ef7ad842034
attila5287 <atiturkoz@hotmail.com>
2019-02-19 06:03:06 -0700

    add MONGODB

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+import pandas as pd
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
+from dataImport import testURLS
+
+# create instance of Flask app
 app = Flask(__name__)
+mongo_uri = 'mongodb://heroku_x58zdhbn:fb7o0k7stbsk0ivbirarr2i2d3@ds139775.mlab.com:39775/heroku_x58zdhbn'
+app.config['MONGO_URI'] = mongo_uri
+flask_debug = False
+app.config['FLASK_DEBUG'] = flask_debug
+# Create db connection
+mongo = PyMongo(app,uri=mongo_uri)
 
 
 @app.route('/')
@@ -9,7 +20,16 @@ def index():
 
 @app.route('/test')
 def simulator():
-    return render_template('test.html')
+  # test data for colorado one bedroom
+  inventoryDemo=pd.read_csv("https://raw.githubusercontent.com/attila5287/pr3-RegroPoly-assets-herokuAPP/master/testData.csv", encoding='ISO-8859-1')
+
+  invenDemo = inventoryDemo.copy()
+
+  print(type(invenDemo))
+
+  return render_template('test.html', inventoryDemo=invenDemo)
+  
+
 
 if __name__ == '__main__':
   app.run(debug=True)
--- functions.py
+++ functions.py
@@ -1,1 +1,4 @@
 import pandas as pd
+
+invDemo=pd.read_csv("static/coloradoTestItemsR0_bed1.csv", encoding='ISO-8859-1')
+
