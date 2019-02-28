52b599a1e1445eb85dbc51e60258ae917d70b8be
attila5287 <atiturkoz@hotmail.com>
2019-02-28 03:12:44 -0700

    index reTouch

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import pandas as pd
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
 import json
-
-# create instance of Flask app
+import jinja2
 app = Flask(__name__)
+# create instance of Flask app
 mongo_uri = 'mongodb://heroku_x58zdhbn:fb7o0k7stbsk0ivbirarr2i2d3@ds139775.mlab.com:39775/heroku_x58zdhbn'
 app.config['MONGO_URI'] = mongo_uri
 flask_debug = False
@@ -15,10 +15,39 @@ mongo = PyMongo(app,uri=mongo_uri)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  pass
+  figureL2st=list()
+  figureL1st = ['plotColorado.png',
+  'plotCalifornia.png',
+  'plotNebraska.png',
+  'custPriceIndex.png',
+  'medianHHI.png',
+  'unempRate.png',
+  'mortgage15.png',
+  'mortgage30.png',
+  'rentCustIndex.png'
+  ]
+  preFigureSrc = 'https://raw.githubusercontent.com/attila5287/pr3-RegroPoly-assets-herokuAPP/master/plot/'
+  figureL2st=[]
+  for file in figureL1st:
+    file = preFigureSrc + file
+    figureL2st.append(file) 
+
+  figureL3st = ['Colorado all homes zillow actual prices, stars are Round Dates:Game Checkpoints',
+  'California all homes zillow actual prices, stars are Round Dates:Game Checkpoints', 
+  'Nebraska all homes zillow actual prices, stars are Round Dates:Game Checkpoints', 
+  'Macro-econ: Pair Plot for House Prices vs Customer Price Index',
+  'Macro-econ: Pair Plot for House Prices vs Median HouseHold Income',
+  'Macro-econ: Pair Plot for House Prices vs Unemployment Rate',
+  'Macro-econ: Pair Plot for House Prices vs Mortgage 15 yrs rate',
+  'Macro-econ: Pair Plot for House Prices vs Mortgage 30 yrs rate',
+  'Macro-econ: Pair Plot for House Prices vs Customer Rent Index'
+  ]
+
+  return render_template('index.html',figureList=figureL2st, descList=figureL3st)
 
 
-@app.route('/test')
+@app.route('/test') 
 def simulator():
   # test data for colorado one bedroom
   readDataRaw_df=pd.read_csv("https://raw.githubusercontent.com/attila5287/pr3-RegroPoly-assets-herokuAPP/master/testData.csv", encoding='ISO-8859-1')
@@ -31,6 +60,7 @@ def simulator():
   return render_template('test.html', inventory=inventory)
   
 # ===============================
+
 
 @app.route('/assets')
 def assets():
@@ -55,3 +85,5 @@ def houseForSale():
 
 if __name__ == '__main__':
   app.run(debug=True)
+
+
