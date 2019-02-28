d3557b43a5d3de1ffb686203c5e68fc02591c2bb
attila5287 <atiturkoz@hotmail.com>
2019-02-27 18:20:59 -0700

    final push before prez

--- app.py
+++ app.py
@@ -28,9 +28,29 @@ def simulator():
   mongo.db.collection.insert_many(dictBeforeMongo)
   inventory = list(mongo.db.collection.find())
 
-
   return render_template('test.html', inventory=inventory)
   
+# ===============================
+
+@app.route('/assets')
+def assets():
+  assetsRaw_df=pd.read_csv("https://raw.githubusercontent.com/attila5287/pr3-RegroPoly-assets-herokuAPP/master/coloradoTestReg_bed1.csv", encoding='ISO-8859-1')
+  dictAssets = assetsRaw_df.to_dict('records')
+  mongo.db.collectionAssets.drop()
+  mongo.db.collectionAssets.insert_many(dictAssets)
+  listAssets = list(mongo.db.collectionAssets.find())
+
+  return render_template('testAssets.html',assets=listAssets)
+
+@app.route('/housemarket')
+def houseForSale():
+  forSaleRaw_df=pd.read_csv("https://raw.githubusercontent.com/attila5287/pr3-RegroPoly-assets-herokuAPP/master/testRunSimulatedNumbers.csv  ", encoding='ISO-8859-1')
+  dictHouseMarket = forSaleRaw_df.to_dict('records')
+  mongo.db.collectionHouseMarket.drop()
+  mongo.db.collectionHouseMarket.insert_many(dictHouseMarket)
+  listHouseProperties = list(mongo.db.collectionHouseMarket.find())
+
+  return render_template('testHouseMarket.html',houseForSaleListing=listHouseProperties)
 
 
 if __name__ == '__main__':
